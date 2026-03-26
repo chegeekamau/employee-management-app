7370defa514f4472dd518a66a05748b596dd5a35
chegeekamau <josephchege@kabarak.ac.ke>
2026-03-26 10:40:14 +0300

    few changes

---
+++ main.js
@@ -0,0 +1,15 @@
+function details(){
+    alert("Diplaying full employee details");
+} 
+function editContact(){
+    alert("Edit employee information");
+}
+function deleteContact(){
+    let confirmDelete = confirm("Are you sure you want to delete");
+
+    if(confirmDelete){
+        alert("Employee delete")
+    }
+
+
+}
